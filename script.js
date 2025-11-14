1b73da5ee8da9b810c0c254fedb30d415a64f153
Khanh Nguyen <96412559+qilskcter@users.noreply.github.com>
2025-11-14 18:27:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ window.addEventListener("load", () => {
 
 window.addEventListener('load', () => {
     const preloader = document.querySelector('.preloader');
-    
+
     if (preloader) {
         preloader.classList.add('hidden');
     }
@@ -244,7 +244,7 @@ async function getCurrentlyPlaying() {
     try {
         const timestamp = Math.floor(Date.now() / 1000).toString();
         
-        const response = await fetch("/api/spotify", {
+        const response = await fetch("/api/get-music", {
             method: "GET",
             headers: {
                 "x-timestamp": timestamp
